ec06c295d9fc421eaf4f53306ea4e89bc6573a88
Xuanli Lin <l19980623@gmail.com>
2019-10-21 15:56:25 -0700

    stub for #33

--- src/ConcatenatedWords.java
+++ src/ConcatenatedWords.java
@@ -1,37 +1,21 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+
+/**
+ * #472 Concatenated Words https://leetcode.com/problems/concatenated-words/
+ */
 
 public class ConcatenatedWords {
-    public List<String> findAllConcatenatedWordsInADict(String[] words) {
+    private formable(String word, Set<String> dict) {
+
+    }
+
+    public static List<String> findAllConcatenatedWordsInADict(String[] words) {
         List<String> result = new ArrayList<>();
-        if (words == null || words.length < 3) return result;
+        Set<String> prefix = new HashSet<>();
+        Arrays.sort(words, Comparator.comparingInt(String::length));
 
-        TrieNode root = new TrieNode(' ', false);
-        TrieNode current = root;
-
-        for (String word : words) {
+        for (int i = 0; i < words.length; i++) {
 
         }
-    }
-
-    private void addToTrie(TrieNode root, String s) {
-        for (int i = 0; i < s.length(); i++) {
-
-        }
-    }
-
-}
-
-class TrieNode {
-    Map<Character, TrieNode> children;
-    boolean isEnd;
-    char c;
-
-    TrieNode(char c, boolean isEnd) {
-        children = new HashMap<>();
-        this.isEnd = isEnd;
-        this.c = c;
     }
 }
