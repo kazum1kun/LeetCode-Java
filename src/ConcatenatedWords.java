d7e5094a1bf4fd946d329c674b3323d50f9e6b3b
Xuanli Lin <l19980623@gmail.com>
2019-10-22 13:27:49 -0700

    #472

--- src/ConcatenatedWords.java
+++ src/ConcatenatedWords.java
@@ -2,20 +2,40 @@ import java.util.*;
 
 /**
  * #472 Concatenated Words https://leetcode.com/problems/concatenated-words/
+ * 282ms/46.8MB (Beats 8.96%/95.24%)
  */
 
 public class ConcatenatedWords {
-    private formable(String word, Set<String> dict) {
+    private boolean formable(String word, Set<String> dict) {
+        if (dict.isEmpty()) return false;
 
+        boolean[] formableFrom = new boolean[word.length() + 1];
+        formableFrom[0] = true;
+
+        for (int i = 1; i <= word.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (!formableFrom[j]) continue;
+                if (dict.contains(word.substring(j, i))) {
+                    formableFrom[i] = true;
+                    break;
+                }
+            }
+        }
+        return formableFrom[word.length()];
     }
 
-    public static List<String> findAllConcatenatedWordsInADict(String[] words) {
+    public List<String> findAllConcatenatedWordsInADict(String[] words) {
         List<String> result = new ArrayList<>();
         Set<String> prefix = new HashSet<>();
         Arrays.sort(words, Comparator.comparingInt(String::length));
 
-        for (int i = 0; i < words.length; i++) {
-
+        for (String word : words) {
+            if (formable(word, prefix)) {
+                result.add(word);
+            }
+            prefix.add(word);
         }
+
+        return result;
     }
 }
